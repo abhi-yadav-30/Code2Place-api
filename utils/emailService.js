106d474bb81fb17f9b73eb32bc51e58b4fd3af6f
Abhinandan Yadav <cs21b1011@iiitdm.ac.in>
2026-02-15 00:13:30 +0530

    jbs

--- utils/emailService.js
+++ utils/emailService.js
@@ -9,11 +9,16 @@ const getTransporter = () => {
       return null;
     }
     transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
+       host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
     });
   }
   return transporter;
